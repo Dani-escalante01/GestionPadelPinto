70d8d753a0a0aa7bc8120b859ac0394e055cf753
carlossuarez <carsuagon@alu.edu.gva.es>
2026-02-19 12:11:17 +0100

    arreglo de permisos en backend.php (a ver si así va)

--- server/backend.php
+++ server/backend.php
@@ -17,16 +17,8 @@ $action = isset($_GET['action']) ? $_GET['action'] : null;
 $DB_FILE = __DIR__ . '/padel_pro_v2.db';
 
 try {
-    $db = new SQLite3($DB_FILE);
-    $db->enableExceptions(true);
-    
-    if (file_exists($DB_FILE)) {
-        @chmod($DB_FILE, 0666); 
-    }
-    
-    $db->exec("PRAGMA journal_mode = TRUNCATE;"); 
-    $db->exec("PRAGMA busy_timeout = 5000;"); 
-    
+    $db = new SQLite3($DB_FILE, SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+    $db->enableExceptions(true);    
 } catch (Exception $e) {
     jsonResponse(array("error" => "Error de conexión: " . $e->getMessage()), 500);
 }
